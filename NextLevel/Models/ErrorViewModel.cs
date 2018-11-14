8dc57a26362b47ce4277acbc96f4eba3f8b44d34
Derikos <jokermc01@gmail.com>
2018-11-14 10:26:03 +0200

    Add project files.

---
+++ NextLevel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NextLevel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
